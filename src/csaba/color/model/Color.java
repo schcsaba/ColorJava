3cd8a440730ed0042d6eb65e00ab8921439985f6
Csaba Schnitchen <schcsaba@gmail.com>
2023-06-26 15:12:03 +0200

    Initial commit

---
+++ src/csaba/color/model/Color.java
@@ -0,0 +1,77 @@
+package csaba.color.model;
+
+public class Color {
+
+    private int red;
+    private int green;
+    private int blue;
+    private String hexValue;
+    public Color(int red, int green, int blue) {
+        checkColorIntValue(red);
+        checkColorIntValue(green);
+        checkColorIntValue(blue);
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        hexValue = String.format("#%02X%02X%02X", red, green, blue);
+    }
+
+    public Color(String hexValue) {
+        this.hexValue =  hexValue;
+        red = Integer.valueOf(hexValue.substring(1, 3), 16);
+        green = Integer.valueOf(hexValue.substring(3, 5), 16);
+        blue = Integer.valueOf(hexValue.substring(5, 7), 16);
+    }
+
+    @Override
+    public String toString() {
+        return "[value=" + hexValue + ", r=" + red + ", g=" + green + ", b=" + blue + "]";
+    }
+
+    public String getHexValue() {
+        return hexValue;
+    }
+
+    public void setHexValue(String hexValue) {
+        this.hexValue = hexValue;
+        red = Integer.valueOf(hexValue.substring(1, 3), 16);
+        green = Integer.valueOf(hexValue.substring(3, 5), 16);
+        blue = Integer.valueOf(hexValue.substring(5, 7), 16);
+    }
+
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        checkColorIntValue(red);
+        this.red = red;
+        hexValue = String.format("#%02X%02X%02X", red, green, blue);
+    }
+
+    public int getGreen() {
+        return green;
+    }
+
+    public void setGreen(int green) {
+        checkColorIntValue(green);
+        this.green = green;
+        hexValue = String.format("#%02X%02X%02X", red, green, blue);
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+
+    public void setBlue(int blue) {
+        checkColorIntValue(blue);
+        this.blue = blue;
+        hexValue = String.format("#%02X%02X%02X", red, green, blue);
+    }
+
+    private void checkColorIntValue(int color) {
+        if (color < 0 || color > 255) {
+            throw new IllegalArgumentException("The color value should be between 0 and 255.");
+        }
+    }
+}
---
+++ test/csaba/color/model/ColorTest.java
@@ -0,0 +1,129 @@
+package csaba.color.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ColorTest {
+    private Color colorRGBInt;
+    private Color colorRGBHex;
+
+    @BeforeEach
+    public void setUp() {
+        colorRGBInt = new Color(10, 50, 150);
+        colorRGBHex = new Color("#0A3296");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        colorRGBInt = null;
+        colorRGBHex =  null;
+    }
+
+    @Test
+    public void test_get_red() {
+        assertEquals(10, colorRGBInt.getRed());
+        assertEquals(50, colorRGBInt.getGreen());
+        assertEquals(150, colorRGBInt.getBlue());
+        assertEquals("#0A3296", colorRGBInt.getHexValue());
+        assertEquals(10, colorRGBHex.getRed());
+        assertEquals(50, colorRGBHex.getGreen());
+        assertEquals(150, colorRGBHex.getBlue());
+        assertEquals("#0A3296", colorRGBHex.getHexValue());
+    }
+
+    @Test
+    public void test_set_red() {
+        colorRGBInt.setRed(20);
+        colorRGBHex.setRed(20);
+        assertEquals(20, colorRGBInt.getRed());
+        assertEquals(50, colorRGBInt.getGreen());
+        assertEquals(150, colorRGBInt.getBlue());
+        assertEquals("#143296", colorRGBInt.getHexValue());
+        assertEquals(20, colorRGBHex.getRed());
+        assertEquals(50, colorRGBHex.getGreen());
+        assertEquals(150, colorRGBHex.getBlue());
+        assertEquals("#143296", colorRGBHex.getHexValue());
+    }
+
+    @Test
+    public void test_set_green() {
+        colorRGBInt.setGreen(60);
+        colorRGBHex.setGreen(60);
+        assertEquals(10, colorRGBInt.getRed());
+        assertEquals(60, colorRGBInt.getGreen());
+        assertEquals(150, colorRGBInt.getBlue());
+        assertEquals("#0A3C96", colorRGBInt.getHexValue());
+        assertEquals(10, colorRGBHex.getRed());
+        assertEquals(60, colorRGBHex.getGreen());
+        assertEquals(150, colorRGBHex.getBlue());
+        assertEquals("#0A3C96", colorRGBHex.getHexValue());
+    }
+
+    @Test
+    public void test_set_blue() {
+        colorRGBInt.setBlue(160);
+        colorRGBHex.setBlue(160);
+        assertEquals(10, colorRGBInt.getRed());
+        assertEquals(50, colorRGBInt.getGreen());
+        assertEquals(160, colorRGBInt.getBlue());
+        assertEquals("#0A32A0", colorRGBInt.getHexValue());
+        assertEquals(10, colorRGBHex.getRed());
+        assertEquals(50, colorRGBHex.getGreen());
+        assertEquals(160, colorRGBHex.getBlue());
+        assertEquals("#0A32A0", colorRGBHex.getHexValue());
+    }
+
+    @Test
+    public void test_set_hex_value() {
+        colorRGBInt.setHexValue("#0A32A0");
+        colorRGBHex.setHexValue("#0A32A0");
+        assertEquals(10, colorRGBInt.getRed());
+        assertEquals(50, colorRGBInt.getGreen());
+        assertEquals(160, colorRGBInt.getBlue());
+        assertEquals("#0A32A0", colorRGBInt.getHexValue());
+        assertEquals(10, colorRGBHex.getRed());
+        assertEquals(50, colorRGBHex.getGreen());
+        assertEquals(160, colorRGBHex.getBlue());
+        assertEquals("#0A32A0", colorRGBHex.getHexValue());
+    }
+
+    @Test
+    public void test_to_string() {
+        assertEquals("[value=#0A3296, r=10, g=50, b=150]", colorRGBInt.toString());
+        assertEquals("[value=#0A3296, r=10, g=50, b=150]", colorRGBHex.toString());
+    }
+
+    @Test
+    public void test_set_red_with_invalid_value() {
+        assertThrows(IllegalArgumentException.class, () -> colorRGBInt.setRed(-20));
+        assertThrows(IllegalArgumentException.class, () -> colorRGBHex.setRed(-20));
+    }
+
+    @Test
+    public void test_set_green_with_invalid_value() {
+        assertThrows(IllegalArgumentException.class, () -> colorRGBInt.setGreen(-20));
+        assertThrows(IllegalArgumentException.class, () -> colorRGBHex.setGreen(-20));
+    }
+
+    @Test
+    public void test_set_blue_with_invalid_value() {
+        assertThrows(IllegalArgumentException.class, () -> colorRGBInt.setBlue(-20));
+        assertThrows(IllegalArgumentException.class, () -> colorRGBHex.setBlue(-20));
+    }
+
+    @Test
+    public void test_set_hex_value_with_invalid_value() {
+        assertThrows(IllegalArgumentException.class, () -> colorRGBInt.setHexValue("$MS00AA"));
+        assertThrows(IllegalArgumentException.class, () -> colorRGBHex.setHexValue("$MS00AA"));
+    }
+
+    @Test
+    public void test_constructors() {
+        assertThrows(IllegalArgumentException.class, () -> new Color(300, 50, 150));
+        assertThrows(IllegalArgumentException.class, () -> new Color("$MS00AA"));
+    }
+}
